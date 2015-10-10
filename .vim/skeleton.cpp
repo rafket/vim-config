6adf4a7a95e38ff620a12ed0cf8b471cef1c8c32
Rafael Ketsetsides <rafael859@gmail.com>
2015-10-10 12:52:17 +0300

    cpp skeleton file

---
+++ .vim/skeleton.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <map>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+#define X first
+#define Y second
+#define mp make_pair
+#define pb push_back
+#define piii pair<pair<int,int>,int>
+#define pii pair<int,int>
+#define pdd pair<double,double>
+
+void Debug()
+{
+}
+
+void Read()
+{
+}
+
+void Solve()
+{
+}
+
+int main()
+{
+    Read();
+    Solve();
+    Debug();
+    return 0;
+}
